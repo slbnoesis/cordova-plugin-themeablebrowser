16b7db3f3a281f10d3d0f7582ea3a871cef001c1
zmoreira <zmoreira@users.noreply.github.com>
2017-06-21 10:32:37 +0100

    Set user agent

--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -666,6 +666,10 @@
 {
     // We create the views in code for primarily for ease of upgrades and not requiring an external .xib to be included
 
+    //Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53
+   // zmoreira - set user agent 
+ NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"Mozilla/5.0 (iPhone; CPU iPhone OS 7_0_2 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A501 Safari/9537.53", @"UserAgent", nil];
+[[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
     CGRect webViewBounds = self.view.bounds;
     //BOOL toolbarIsAtBottom = ![_browserOptions.toolbarposition isEqualToString:kThemeableBrowserToolbarBarPositionTop];
     BOOL toolbarIsAtBottom = true;
